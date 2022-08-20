c9179b9d12223a6bf51d2243e7263f332cd8c193
Farabi9 <ifatkhan633@gmail.com>
2022-08-21 02:28:06 +0600

    cf

--- index.js
+++ index.js
@@ -44,6 +44,4 @@ app.get('/', (req, res) => {
   res.send('Hello ED-Tech!')
 })
 
-app.listen(port, () => {
-  console.log(` Listening on port ${port}`)
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
